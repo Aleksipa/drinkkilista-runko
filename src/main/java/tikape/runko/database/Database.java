ea698dbfcf288e94bbd76be5398e911a0f4ebdba
Pikipum <markus.tervonen@hotmail.com>
2018-02-25 22:14:17 +0200

    pari loitsua postgresql varten

--- src/main/java/tikape/runko/database/Database.java
+++ src/main/java/tikape/runko/database/Database.java
@@ -12,7 +12,15 @@ public class Database {
         this.databaseAddress = databaseAddress;
     }
 
-    public Connection getConnection() throws SQLException {
+    /*  public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(databaseAddress);
+    } */
+    public Connection getConnection() throws Exception {
+        String dbUrl = System.getenv("JDBC_DATABASE_URL");
+        if (dbUrl != null && dbUrl.length() > 0) {
+            return DriverManager.getConnection(dbUrl);
+        }
+
         return DriverManager.getConnection(databaseAddress);
     }
 
